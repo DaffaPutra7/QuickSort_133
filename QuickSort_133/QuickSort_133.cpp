42931cdc4ac4e374d0c26558d21dfc0608ddc010
DaffaPutra7 <putradika3604@gmail.com>
2023-03-30 15:54:06 +0700

    int main(Finish)

--- QuickSort_133/QuickSort_133.cpp
+++ QuickSort_133/QuickSort_133.cpp
@@ -111,3 +111,12 @@ void display() {
 	cout << "Number of data movements: " << mov_count << endl;
 }
 
+int main()
+{
+	input();
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
